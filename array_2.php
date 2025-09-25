5a7ec8eaf62000a761b57d98170948caff6e3d80
wwidi <wwidi4087@gmail.com>
2025-09-25 19:02:01 +0700

    update array_2.php internal style

--- array_2.php
+++ array_2.php
@@ -14,3 +14,56 @@ echo $Listdosen[1] . "<br>";
 ?>
 </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Dosen</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            padding: 20px;
+        }
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin-top: 10px;
+        }
+        th, td {
+            border: 1px solid #444;
+            padding: 8px 12px;
+        }
+        th {
+            background-color: #f2f2f2;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdana',
+            'domisili' => 'Malang',
+            'jenis_kelamin' => 'Perempuan'
+        ];
+    ?>
+
+    <h3>Data Dosen</h3>
+    <table>
+        <tr>
+            <th>Nama</th>
+            <td><?php echo $Dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <th>Domisili</th>
+            <td><?php echo $Dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <th>Jenis Kelamin</th>
+            <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
