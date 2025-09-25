23821d53f095ff844a0941aea13843b74d9630a0
wwidi <wwidi4087@gmail.com>
2025-09-25 19:22:59 +0700

    modif function.php

--- function.php
+++ function.php
@@ -11,3 +11,17 @@ perkenalan();
 
 ?>
 
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Widiii<br>"; //Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+echo "<hr>"; // Tambahkan garis pemisah agar outputnya jelas
+perkenalan(); // Panggilan kedua
+
+?>
