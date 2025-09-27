598512c4c4aee132ff038938fabd16db98768560
wwidi <wwidi4087@gmail.com>
2025-09-27 13:26:50 +0700

    tanpa loop for function.php

--- loop for function.php
+++ loop for function.php
@@ -107,3 +107,14 @@ for ($i=1; $i <=25; $i++){
     echo "Perulangan ke-{$i} <br>";
 }
 ?>
+
+<?php
+function tampilkanAngka(int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+    // Panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
+?>
